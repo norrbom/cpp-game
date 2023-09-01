cf75ad4060abcf0d459c3e5a68081011b460538e
Anders Norrbom <anders@norrbom.org>
2023-09-01 09:21:06 +0200

    set speed

--- src/modules/sprite.h
+++ src/modules/sprite.h
@@ -5,41 +5,32 @@ class Sprite
 {
     public:
         // p must be a pointer returned by new; it must not be NULL
-        Sprite(sf::Shape* s): shape(s), direction(rnd(-1, 1), rnd(-1, 1)) {
+        Sprite(sf::Shape* shape, float speed):
+            shape(shape),
+            speed(speed),
+            direction(rnd(-1, 1), rnd(-1, 1))
+        {
             assert(shape != NULL);
             shape->setFillColor(randColor());
         }
         sf::Shape* shape;
-        sf::Shape* move(float deltaTime, float speed, sf::Vector2i* w) {
+        float speed;
+        sf::Shape* move(float deltaTime, sf::Vector2i* w) {
 
-            if (touchedGround(shape, (*w).y)) {
+            if (touchedGround(shape, (*w).y) || touchedCeiling(shape)) {
                 shape->setFillColor(randColor());
-                direction.y = -1.0;
+                direction.y = direction.y * -1.0f;
             }
-            if (touchedCeiling(shape)) {
+            if (touchedRightWall(shape, (*w).x) || touchedLeftWall(shape)) {
                 shape->setFillColor(randColor());
-                direction.y = 1.0;
+                direction.x = direction.x * -1.0f;
             }
-            if (touchedRightWall(shape, (*w).x)) {
-                shape->setFillColor(randColor());
-                direction.x = -1.0;
-            }
-            if (touchedLeftWall(shape)) {
-                shape->setFillColor(randColor());
-                direction.x = 1.0;
-            }
-
-            sf::Vector2f movement(0.0f, 0.0f);
             float distance = speed * deltaTime;
-            movement.x = distance * direction.x;
-            movement.y = distance * direction.y;
-
-            shape->move(movement);
+            shape->move(sf::Vector2f(distance * direction.x, distance * direction.y));
             return shape;
         }
     private:
         sf::Vector2f direction;
-
         float rnd(int min, int max) {
             // construct a trivial random generator engine from a time-based seed:
             RangeRandGenerator generator(min, max);
