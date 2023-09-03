98ce6e1e42b7898db88555c2a66eb9e427c64c8c
Anders Norrbom <anders@norrbom.org>
2023-09-03 23:21:55 +0200

    window funciton

---
+++ src/modules/window.h
@@ -0,0 +1,16 @@
+using namespace std;
+
+sf::Vector2i createWindow(sf::RenderWindow * window, int x, int y, string title, int style) {
+    if (style != sf::Style::Fullscreen) {
+        window->create(sf::VideoMode(x, y), title, style);
+    }
+    else {
+        window->create(sf::VideoMode::getFullscreenModes()[0], title, style);
+    }
+    window->setFramerateLimit(60);
+    sf::Image icon;
+    if (!icon.loadFromFile("resources/scull-icon.png"))
+        throw std::runtime_error {"Could not load icon file"};
+    window->setIcon(icon.getSize().x, icon.getSize().y, icon.getPixelsPtr());
+    return sf::Vector2i(window->getSize().x, window->getSize().y);
+}
