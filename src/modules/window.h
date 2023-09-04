08e70934dd0ca9e812ec8090bab879f7b4a68f13
Anders Norrbom <anders@norrbom.org>
2023-09-04 08:57:34 +0200

    set fps

--- src/modules/window.h
+++ src/modules/window.h
@@ -1,13 +1,13 @@
 using namespace std;
 
-sf::Vector2i createWindow(sf::RenderWindow * window, int x, int y, string title, int style) {
+sf::Vector2i createWindow(sf::RenderWindow * window, int x, int y, unsigned fps, string title, int style) {
     if (style != sf::Style::Fullscreen) {
         window->create(sf::VideoMode(x, y), title, style);
     }
     else {
         window->create(sf::VideoMode::getFullscreenModes()[0], title, style);
     }
-    window->setFramerateLimit(60);
+    window->setFramerateLimit(fps);
     sf::Image icon;
     if (!icon.loadFromFile("resources/scull-icon.png"))
         throw std::runtime_error {"Could not load icon file"};
