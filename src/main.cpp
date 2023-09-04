51e71e5c7ad44015284e2b9ea526d647098569fb
Anders Norrbom <anders@norrbom.org>
2023-09-04 08:57:24 +0200

    tiles

--- src/main.cpp
+++ src/main.cpp
@@ -5,40 +5,62 @@
 
 using namespace std;
 
+struct Tile
+{
+        sf::RectangleShape shape;
+};
+
 int main(const int argc, const char* argv[])
 {
 	// General settings
 	sf::Clock clock;
 	float fpsDeltaTime = 0.0;
-	string stats = "FPS:  0";
-	string GAME_TITLE = "Dungeon Escape";
+	constexpr unsigned FPS = 60;
+	unsigned fps = 0;
 	float GRAVITY = 980.0f; // cm/s
-	sf::Vector2f initVelocity(rnd(100.0f, 100.0f), rnd(100.0f, 100.0f));
-	int fps = 0;
+	string stats = "FPS: " + to_string(FPS);
+	string GAME_TITLE = "Dungeon Escape";
 
-	/* Initialize window */
+	sf::Vector2f initVelocity(rnd(100.0f, 100.0f), rnd(100.0f, 100.0f));
+
+	/* Initialize window and tiles */
 	sf::RenderWindow window;
-	sf::Vector2i win = createWindow(&window, 1024, 768, GAME_TITLE, sf::Style::Default);
+	sf::Vector2i win = createWindow(&window, 1024, 768, FPS, GAME_TITLE,  sf::Style::Default);
+	sf::Vector2f TILE_SIZE{ (float)win.x/20,(float)win.x/20 };
+	float unitRadius = (float)win.x/40;
+	std::vector<Tile> tilemap;
+	for (auto y = 0; y < win.y / TILE_SIZE.y; y++)
+	{
+        for (auto x = 0; x < win.x / TILE_SIZE.x; x++)
+		{
+			auto tile = new Tile();
+			tile->shape.setSize( {TILE_SIZE.x,TILE_SIZE.y} );
+			tile->shape.setOutlineThickness(0.5f);
+			tile->shape.setOutlineColor(sf::Color(255, 125, 125, 32));
+			tile->shape.setFillColor(sf::Color(125, 125, 125, 32));
+			tile->shape.setPosition(x * TILE_SIZE.x, y * TILE_SIZE.y);
+			tilemap.push_back(*tile);
+		}
+	}
 
 	/* Initialize objects */
-	sf::CircleShape characterShape(20.f);
+	sf::CircleShape characterShape(unitRadius);
 	characterShape.setOrigin(characterShape.getRadius() , characterShape.getRadius());
 	characterShape.setFillColor(sf::Color(150, 50, 250));
 	float r = characterShape.getRadius();
 	Sprite character(&characterShape, sf::Vector2f((float)win.x/2, (float)win.y/2), initVelocity, GRAVITY);
 
-	float radius = 40.f;
 	int count = 4;
 	vector<Sprite> sprites;
 	vector<sf::CircleShape> circles;
 	for (int i = 0; i < count; ++i) {
-		circles.push_back(sf::CircleShape(radius));
+		circles.push_back(sf::CircleShape(unitRadius));
 	}
 	for (int i = 0; i < count; ++i) {
-		sprites.push_back(Sprite(&circles[i], sf::Vector2f(rnd(radius, (float)win.x - radius), rnd(radius, (float)win.y- radius)), sf::Vector2f(rnd(100.0f, 400.0f), rnd(100.0f, 400.0f)), GRAVITY));
+		sprites.push_back(Sprite(&circles[i], sf::Vector2f(rnd(unitRadius, (float)win.x - unitRadius), rnd(unitRadius, (float)win.y/3)), sf::Vector2f(rnd(100.0f, 400.0f), rnd(100.0f, 400.0f)), GRAVITY));
 		sprites[i].bounciness = 0.70f;
 		sprites[i].friction = 0.99f;
-		sprites[i].shape->setOrigin(radius , radius);
+		sprites[i].shape->setOrigin(sprites[i].shape->getRadius(), sprites[i].shape->getRadius());
 	}
 
 	sf::Font font;
@@ -62,7 +84,7 @@ int main(const int argc, const char* argv[])
 				window.close();
 			if (event.key.code == sf::Keyboard::F)
 			{
-				win = createWindow(&window, 0, 0, GAME_TITLE, sf::Style::Fullscreen);
+				win = createWindow(&window, 0, 0, FPS, GAME_TITLE, sf::Style::Fullscreen);
 			}
 
 			// Control character, set full speed and direction
@@ -80,11 +102,19 @@ int main(const int argc, const char* argv[])
 			}
 		}
 
-		window.clear(sf::Color::Black);
+
 
 		if (fpsDeltaTime > 0.5) {
 			fps = int(1/deltaTime);
 			fpsDeltaTime = 0.0;
+		}
+
+		/* Draw */
+
+		window.clear(sf::Color::Black);
+
+		for (auto& it : tilemap) {
+				window.draw(it.shape);
 		}
 
 		for (int i = 0; i < sprites.size(); i++)
